f112c9688e56dfb413b97367942ff3599583878c
Ye0ngjin <104483761+Ye0ngjin@users.noreply.github.com>
2023-11-30 04:49:07 +0000

    _site 다시 추가

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "블로그 시작!!",
+        "excerpt":"우선은 테스트 글  테스트  테스트  테스트  테스트  테스트  테스트  테스트  테스트  테스트  테스트  테스트  테스트  ","categories": ["Blog"],
+        "tags": ["Blog","Github","Git"],
+        "url": "https://ye0ngjin.github.io/blog-before-changing-theme/blog/my-first-post/",
+        "teaser": null
+      },{
+        "title": "깃허브페이지에 블로그 올리기",
+        "excerpt":"[Github 블로그] 블로그 포스팅하는 방법과 GitHub 블로그 시작하기를 참고하였습니다. 첫번째 블로그의 깃허브 저장소|두번째 블로그의 깃허브 저장소 준비되어야 할 사항 1. 루비, 지킬, 번들러 루비 - ruby-lang.org/ko 지킬 - jekyllrb.com 또는 jekyllrb-ko.github.io 번들러 - bundler.io 루비를 설치하고 터미널에서 아래의 명령어를 입력한다. gem install jekyll bundler Mac을 사용중이라면 가상환경을 이용하여 루비를 설치하는...","categories": ["Blog"],
+        "tags": ["Blog","jekyll","minimal-mistake","Git","Github"],
+        "url": "https://ye0ngjin.github.io/blog-before-changing-theme/blog/how-to/",
+        "teaser": null
+      }]
